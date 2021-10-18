942b95be3011dffd99651730f85dbf4f6007fd14
RookieQaz <92728962+RookieQaz@users.noreply.github.com>
2021-10-18 18:20:54 +0600

    Delete emoji.js

--- emoji.js
+++
@@ -1,9 +0,0 @@
-// Create the panel before opening the dialog for quick opening
-// Though lazy creation also works and is blazing fast! (3ms)
-new EmojiPanel(document.getElementById('example-3'));
-
-// Toggle dialog
-document.getElementById('example-3-btn').addEventListener('click', () => {
-  document.getElementById('example-3-container').classList.toggle('open');
-});
-    
